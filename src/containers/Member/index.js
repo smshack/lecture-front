9895a63a0242aae5f8cce03cd4f6cc62234356db
smshack <5432tat@naver.com>
2022-11-28 23:00:48 +0900

    3. 라우팅으로 페이지 나누기

---
+++ src/containers/Member/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Member = props => {
+  return (
+    <div>Member</div>
+  )
+}
+
+Member.propTypes = {}
+
+export default Member
--- src/containers/RouterApp.js
+++ src/containers/RouterApp.js
@@ -1,6 +1,19 @@
+import { Switch, Route } from "react-router-dom";
+import NotFound from "../components/NotFound";
+
 import "../styles/style.scss";
+import Home from "./Home";
+import Member from "./Member";
 const RouterApp = () => {
-  return <div className="RouterApp">시작</div>;
+  return (
+    <div className="container-wrapper">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/member" component={Member} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 };
 
 export default RouterApp;
