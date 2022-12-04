aabf831e3066c06531567578da6fc6e6ddb6fc66
smshack <5432tat@naver.com>
2022-12-04 20:45:39 +0900

    5. usememo를 사용하기 전에 비교를 위한 그냥 함수 사용

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ https://ko.reactjs.org/docs/hooks-reference.html#usestate
 ## useEffect
 https://ko.reactjs.org/docs/hooks-reference.html#useeffect
 
+### useMemo와 useCallback을 배우기 전에 알아야 하는 것
+- 함수형 컴포넌트는 그냥 함수다. 다시 한 번 강조하자면 함수형 컴포넌트는 단지 jsx를 반환하는 함수이다.
+
+- 컴포넌트가 렌더링 된다는 것은 누군가가 그 함수(컴포넌트)를 호출하여서 실행되는 것을 말한다. 함수가 실행될 때마다 내부에 선언되어 있던 표현식(변수, 또다른 함수 등)도 매번 다시 선언되어 사용된다.
+
+- 컴포넌트는 자신의 state가 변경되거나, 부모에게서 받는 props가 변경되었을 때마다 리렌더링 된다. (심지어 하위 컴포넌트에 최적화 설정을 해주지 않으면 부모에게서 받는 props가 변경되지 않았더라도 리렌더링 되는게 기본이다. )
+## useMemo
+- https://ko.reactjs.org/docs/hooks-reference.html#usememo
+- 메모리제이션(동일한 계산을 반복해야 할 때, 이전에 계산한 값을 메모리에 저장함으로써 동일한 계산의 반복 수행을 제거하여 프로그램 실행 속도를 빠르게 하는 기술)된 값을 반환
+-  useMemo를 사용하지 않고도 동작할 수 있도록 코드를 작성하고 그것을 추가하여 성능을 최적화하는게 더 좋긴 함
+
+## useCallback
+https://ko.reactjs.org/docs/hooks-reference.html#usecallback
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,24 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-const Home = props => {
-    const [modifyValue,setModifyValue] = useState(false)
-    useEffect(() => {
-      console.log(`modifyValue: ${modifyValue} 값`);
-      console.log("modifyValue 값이 변경되면 실행");
-    }, [modifyValue]);
+const Home = (props) => {
+  const [color, setColor] = useState("");
+  const [movie, setMovie] = useState("");
+
+  const onChangeHandler = (e) => {
+    if (e.target.id === "color") setColor(e.target.value);
+    else setMovie(e.target.value);
+  };
+
   return (
-    <div>
-      Home
-      <p>{modifyValue ? "true" : "false"}</p>
-      <button onClick={() => setModifyValue(!modifyValue)}>
-        modifyValue 변경
-      </button>
+    <>
+      <div>
+        <label>
+          What is your favorite color of rainbow ?
+          <input id="color" value={color} onChange={onChangeHandler} />
+        </label>
+      </div>
+      <div>
+        What is your favorite movie among these ?
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Marriage Story"
+            onChange={onChangeHandler}
+          />
+          Marriage Story
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="The Fast And The Furious"
+            onChange={onChangeHandler}
+          />
+          The Fast And The Furious
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="movie"
+            value="Avengers"
+            onChange={onChangeHandler}
+          />
+          Avengers
+        </label>
+        <Info color={color} movie={movie} />
+      </div>
+    </>
+  );
+};
+
+Home.propTypes = {};
+
+export default Home;
+
+const getColorKor = (color) => {
+  console.log("getColorKor");
+  switch (color) {
+    case "red":
+      return "빨강";
+    case "orange":
+      return "주황";
+    case "yellow":
+      return "노랑";
+    case "green":
+      return "초록";
+    case "blue":
+      return "파랑";
+    case "navy":
+      return "남";
+    case "purple":
+      return "보라";
+    default:
+      return "레인보우";
+  }
+};
+
+const getMovieGenreKor = (movie) => {
+  console.log("getMovieGenreKor");
+  switch (movie) {
+    case "Marriage Story":
+      return "드라마";
+    case "The Fast And The Furious":
+      return "액션";
+    case "Avengers":
+      return "슈퍼히어로";
+    default:
+      return "아직 잘 모름";
+  }
+};
+
+const Info = ({ color, movie }) => {
+  const colorKor = getColorKor(color);
+  const movieGenreKor = getMovieGenreKor(movie);
+
+  return (
+    <div className="info-wrapper">
+      제가 가장 좋아하는 색은 {colorKor} 이고, <br />
+      즐겨보는 영화 장르는 {movieGenreKor} 입니다.
     </div>
   );
-  
-}
-
-Home.propTypes = {}
-
-export default Home
+};
