308a44d016f1de9200237746e7b4eb4a0e606238
smshack <5432tat@naver.com>
2022-12-05 22:24:14 +0900

    9. pros 로 값 전달 배열 렌더링

--- README.md
+++ README.md
@@ -1,61 +1,12 @@
-# 8. useRef
-https://ko.reactjs.org/docs/hooks-reference.html#useref
-
--  useRef 로 특정 DOM 선택하기
-- 추가적으로 Video.js, JWPlayer 같은 HTML5 Video 관련 라이브러리, 또는 D3, chart.js 같은 그래프 관련 라이브러리 등의 외부 라이브러리를 사용해야 할 때에도 특정 DOM 에다 적용하기 때문에 DOM 을 선택해야 하는 상황이 발생 할 수 있음
-
+# 9. pros 로 값 전달 배열 렌더링
 
 ```
- const [inputs, setInputs] = useState({
-      name: "",
-      nickname: "",
-    });
-    const nameInput = useRef();
+Home.defaultProps = {
+  name: "초기값",
+};
 
-    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
-    const onChange = (e) => {
-      const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-      console.log(e.target, "e.target");
-      console.log(value, "value");
-      console.log(name, "name");
-      console.log({
-        ...inputs, // 기존의 input 객체를 복사한 뒤
-        [name]: value, // name 키를 가진 값을 value 로 설정
-      });
-      setInputs({
-        ...inputs, // 기존의 input 객체를 복사한 뒤
-        [name]: value, // name 키를 가진 값을 value 로 설정
-      });
-    };
-
-    const onReset = () => {
-      setInputs({
-        name: "",
-        nickname: "",
-      });
-      nameInput.current.focus();
-    };
-
-    return (
-      <div>
-        <input
-          name="name"
-          placeholder="이름"
-          onChange={onChange}
-          value={name}
-          ref={nameInput}
-        />
-        <input
-          name="nickname"
-          placeholder="닉네임"
-          onChange={onChange}
-          value={nickname}
-        />
-        <button onClick={onReset}>초기화</button>
-        <div>
-          <b>값: </b>
-          {name} ({nickname})
-        </div>
-      </div>
+UserList.propTypes = {
+  users:PropTypes.array,
+};
 ```
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,106 +1,56 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
 import PropTypes from "prop-types";
 
-const Home = (props) => {
-  const [color, setColor] = useState("");
-  const [movie, setMovie] = useState("");
-
-  const onChangeHandler = useCallback((e) => {
-    if (e.target.id === "color") setColor(e.target.value);
-    else setMovie(e.target.value);
-  }, []);
-
+const Home = ({name}) => {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
   return (
-    <>
-      <div>
-        <label>
-          What is your favorite color of rainbow ?
-          <input id="color" value={color} onChange={onChangeHandler} />
-        </label>
-      </div>
-      <div>
-        What is your favorite movie among these ?
-        <label>
-          <input
-            type="radio"
-            name="movie"
-            value="Marriage Story"
-            onChange={onChangeHandler}
-          />
-          Marriage Story
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="movie"
-            value="The Fast And The Furious"
-            onChange={onChangeHandler}
-          />
-          The Fast And The Furious
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="movie"
-            value="Avengers"
-            onChange={onChangeHandler}
-          />
-          Avengers
-        </label>
-        <Info color={color} movie={movie} />
-      </div>
-    </>
+     <UserList users={users} />
   );
 };
 
-Home.propTypes = {};
+Home.defaultProps = {
+  name: "초기값",
+};
 
 export default Home;
 
-const getColorKor = (color) => {
-  console.log("getColorKor");
-  switch (color) {
-    case "red":
-      return "빨강";
-    case "orange":
-      return "주황";
-    case "yellow":
-      return "노랑";
-    case "green":
-      return "초록";
-    case "blue":
-      return "파랑";
-    case "navy":
-      return "남";
-    case "purple":
-      return "보라";
-    default:
-      return "레인보우";
-  }
-};
-
-const getMovieGenreKor = (movie) => {
-  console.log("getMovieGenreKor");
-  switch (movie) {
-    case "Marriage Story":
-      return "드라마";
-    case "The Fast And The Furious":
-      return "액션";
-    case "Avengers":
-      return "슈퍼히어로";
-    default:
-      return "아직 잘 모름";
-  }
-};
-
-const Info = ({ color, movie }) => {
-  const colorKor = useMemo(() => getColorKor(color), [color]);
-  const movieGenreKor = useMemo(() => getMovieGenreKor(movie), [movie]);
-
+const User =({ user }) => {
   return (
-    <div className="info-wrapper">
-      제가 가장 좋아하는 색은 {colorKor} 이고, <br />
-      즐겨보는 영화 장르는 {movieGenreKor} 입니다.
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
     </div>
   );
+}
+
+const UserList = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
+}
+
+UserList.propTypes = {
+  users:PropTypes.array,
 };
